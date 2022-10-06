975394d61c5ec7100dc89ac7261779a48e1485aa
lastvv <103041546+lastvv@users.noreply.github.com>
2022-10-06 16:54:56 +0300

    Add files via upload

---
+++ 2/2.py
@@ -0,0 +1,12 @@
+#2.Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
+
+from itertools import accumulate
+import operator
+
+N = int(input('Введите любое число: '))
+
+
+def get_prods(N):
+    return list(accumulate([x for x in range(1, N + 1)], operator.mul))
+
+print(get_prods(N))
